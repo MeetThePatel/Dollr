3f3680d52fec5560ed9e5d94afd9acbd8e0441d1
Meet S. Patel <meet.s.patel.nj@gmail.com>
2019-03-25 03:17:47 -0400

    Reworked the Units.h file to have constructors in the header file instead of the implementation file. Enforced explicit constructors.

--- src/units.cpp
+++ src/units.cpp
@@ -1,21 +1,5 @@
 #include "units.h"
 
-Dollar::Dollar() {
-    value = 0;
-}
-
-Dollar::Dollar(double x) {
-    value = x * 10000;
-}
-
-Dollar::Dollar(int x) {
-    value = x * 10000;
-}
-
-Dollar::Dollar(mpz_class x) {
-    value = x;
-}
-
 Dollar operator+ (const Dollar& x, const Dollar& y) {
     Dollar ret (x.value + y.value);
     return ret;
@@ -38,6 +22,6 @@ Dollar operator/ (const Dollar& x, const Dollar& y) {
 
 std::ostream& operator<< (std::ostream& os, const Dollar& x) {
     mpz_class inter = x.value / mpz_class(100);
-    os << "$" << double(inter.get_si())/100;
+    os << fixed << setprecision(2) << "$" << double(inter.get_si())/100;
     return os;
 }
--- src/units.h
+++ src/units.h
@@ -2,26 +2,28 @@
 #define units_h
 
 #include <iostream>
+#include <iomanip>
 #include <gmp.h>
 #include <gmpxx.h>
+
+using namespace std;
 
 // Because of rounding errors when using Floating-Precision data structures, multiply the
 // dollar amount by 10000
 struct Dollar {
 public:
     mpz_class value;
-    Dollar();
-    Dollar(double);
-    Dollar(int);
-    Dollar(mpz_class);
+    
+    Dollar() : value(0.0){};
+    explicit Dollar(double value) : value(10000 * value){};
+    explicit Dollar(int value) : value(10000 * value){};
+    explicit Dollar(mpz_class value) : value(value){};
     
     friend Dollar operator+ (const Dollar&, const Dollar&);
     friend Dollar operator- (const Dollar&, const Dollar&);
     friend Dollar operator* (const Dollar&, const Dollar&);
     friend Dollar operator/ (const Dollar&, const Dollar&);
     friend std::ostream& operator<< (std::ostream&, const Dollar&);
-    
-    
 };
 
 #endif
